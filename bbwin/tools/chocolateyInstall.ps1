aa8913c462f92077694d961c64ab701f7fa419cd
Michael Smith <smith.michael.oliver@gmail.com>
2016-04-27 21:09:03 +1000

    change install file to install package only
    Install file now installs package only and does not attempt a zip
    install as before.
    Install file uses most up to date chocolatey template

--- bbwin/tools/chocolateyInstall.ps1
+++ bbwin/tools/chocolateyInstall.ps1
@@ -1,44 +1,133 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 
-$packageName = 'bbwin' # arbitrary name for the package, used in messages
-$installerType = 'msi' #only one of these two: exe or msi
-$url = 'http://sourceforge.net/projects/bbwin/files/bbwin/0.13/BBWin_0.13.msi/download' # download url
-$url64 = $url # 64bit URL here or just use the same as $url
-$silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$ErrorActionPreference = 'Stop'; # stop on all errors
 
-# main helpers - these have error handling tucked into them already
-# installer, will assert administrative rights
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-# download and unpack a zip file
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
 
-#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
-  # downloader that the main helpers use to download items
-  #Get-ChocolateyWebFile "$packageName" 'DOWNLOAD_TO_FILE_FULL_PATH' "$url" "$url64"
-  # installer, will assert administrative rights - used by Install-ChocolateyPackage
-  #Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" '_FULLFILEPATH_' -validExitCodes $validExitCodes
-  # unzips a file to the specified location - auto overwrites existing content
-  #Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-  #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
-  # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
-  #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
-  # add specific files as shortcuts to the desktop
-  #$target = Join-Path $MyInvocation.MyCommand.Definition "$($packageName).exe"
-  #Install-ChocolateyDesktopLink $target
-  
-  #------- ADDITIONAL SETUP -------#
-  # make sure to uncomment the error handling if you have additional setup to do
+$packageName= 'bbwin' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://sourceforge.net/projects/bbwin/files/bbwin/0.13/BBWin_0.13.msi/download' # download url
+$url64      = $url # 64bit URL here or remove - if installer is both, use $url
+#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
+#$fileLocation = Join-Path $toolsDir 'SHARE_LOCATION_OF_INSTALLER_FILE'
 
-  #$processor = Get-WmiObject Win32_Processor
-  #$is64bit = $processor.AddressWidth -eq 64
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+  #file         = $fileLocation
 
-  
-  # the following is all part of error handling
-  #Write-ChocolateySuccess "$packageName"
-#} catch {
-  #Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  #throw 
+  #MSI
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+  #OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  #silentArgs   = '/S'           # NSIS
+  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s -u'        # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+                                 #       https://chocolatey.org/packages/autohotkey.portable
+  #validExitCodes= @(0) #please insert other valid exit codes here
+
+  # optional, highly recommended
+  softwareName  = 'bbwin*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '84f489dbe4dc071001d0e680f5d90b02'
+  checksumType  = 'md5' #default is md5, can also be sha1
+  checksum64    = '84f489dbe4dc071001d0e680f5d90b02'
+  checksumType64= 'md5' #default is checksumType
+}
+
+# Check if bbwin is installed already
+#$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+#$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+#$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+#
+#$key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) -ErrorAction SilentlyContinue | ? { $_.DisplayName -like $packageName + "*" }
+
+#if ($key.Count -eq 0) {
+  Install-ChocolateyPackage @packageArgs
+#} elseif ($key.Count -eq 1) {
+  #Write-Warning "Package $packageName seems to be installed already"
+#} elseif ($key.Count -gt 1) {
+  #Write-Warning "$key.Count matches found!"
+  #Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  #Write-Warning "Please alert package maintainer the following keys were matched:"
+  #$key | % {Write-Warning "- $_.DisplayName"}
 #}
+#Install-ChocolateyZipPackage @packageArgs
+# if you are making your own internal packages (organizations), you can embed the installer or
+# put on internal file share and use the following instead (you'll need to add $file to the above)
+#Install-ChocolateyInstallPackage @packageArgs
+
+## Main helper functions - these have error handling tucked into them already
+## see https://github.com/chocolatey/choco/wiki/HelpersReference
+
+## Install an application, will assert administrative rights
+## add additional optional arguments as necessary
+##Install-ChocolateyPackage $packageName $fileType $silentArgs $url [$url64 -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+
+## Download and unpack a zip file
+##Install-ChocolateyZipPackage $packageName $url $toolsDir [$url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+
+## Install Visual Studio Package
+#Install-ChocolateyVsixPackage $packageName $url [$vsVersion] [-checksum $checksum -checksumType $checksumType]
+#Install-ChocolateyVsixPackage @packageArgs
+
+# see the full list at https://github.com/chocolatey/choco/wiki/HelpersReference
+
+# downloader that the main helpers use to download items
+# if removing $url64, please remove from here
+#Get-ChocolateyWebFile $packageName 'DOWNLOAD_TO_FILE_FULL_PATH' $url $url64
+
+# installer, will assert administrative rights - used by Install-ChocolateyPackage
+# use this for embedding installers in the package when not going to community feed or when you have distribution rights
+#Install-ChocolateyInstallPackage $packageName $fileType $silentArgs '_FULLFILEPATH_' -validExitCodes $validExitCodes
+
+# unzips a file to the specified location - auto overwrites existing content
+#Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" $toolsDir
+
+# Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
+#Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
+
+# add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
+#Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
+
+# add specific files as shortcuts to the desktop
+#$target = Join-Path $toolsDir "$($packageName).exe"
+# Install-ChocolateyShortcut -shortcutFilePath "<path>" -targetPath "<path>" [-workDirectory "C:\" -arguments "C:\test.txt" -iconLocation "C:\test.ico" -description "This is the description"]
+
+# outputs the bitness of the OS (either "32" or "64")
+#$osBitness = Get-ProcessorBits
+
+#Install-ChocolateyEnvironmentVariable -variableName "SOMEVAR" -variableValue "value" [-variableType = 'Machine' #Defaults to 'User']
+
+#Install-ChocolateyFileAssociation
+#Install-BinFile ## only use this for non-exe files - chocolatey will automatically pick up the exe files and shim them automatically
+## https://github.com/chocolatey/choco/wiki/CreatePackages#how-do-i-exclude-executables-from-getting-batch-redirects
+
+##PORTABLE EXAMPLE
+#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
+#Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+## END PORTABLE EXAMPLE
+
+## [DEPRECATING] PORTABLE EXAMPLE
+#$binRoot = Get-BinRoot
+#$installDir = Join-Path $binRoot "$packageName"
+#Write-Host "Adding `'$installDir`' to the path and the current shell path"
+#Install-ChocolateyPath "$installDir"
+#$env:Path = "$($env:Path);$installDir"
+
+# if removing $url64, please remove from here
+# despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
+#Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+## END PORTABLE EXAMPLE
